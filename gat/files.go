ebc6665bb97972b1957a438fbd4d842300e5e761
Nathan Youngman <git@nathany.com>
2013-02-23 18:49:02 -0700

    handle architectures in file names

--- gat/files.go
+++ gat/files.go
@@ -5,13 +5,30 @@ import (
 )
 
 var (
-    isTestFile = regexp.MustCompile(`_test\.go$`)
+    isTestFile         = regexp.MustCompile(`_test\.go$`)
+    architectureSuffix = regexp.MustCompile(`(.+)_(386|amd64|arm)$`)
+    osSuffix           = regexp.MustCompile(`(.+)_(darwin|freebsd|linux|netbsd|openbsd|plan9|windows|unix)$`)
 )
 
 func TestFiles(file string) []string {
     if isTestFile.MatchString(file) {
         return []string{file}
     }
-    file = file[:len(file)-3] + "_test.go"
-    return []string{file}
+
+    file = file[:len(file)-3]
+    files := []string{file + "_test.go"}
+
+    matches := architectureSuffix.FindStringSubmatch(file)
+    if len(matches) > 0 {
+        file = matches[1]
+        files = append(files, file+"_test.go")
+    }
+
+    matches = osSuffix.FindStringSubmatch(file)
+    if len(matches) > 0 {
+        file = matches[1]
+        files = append(files, file+"_test.go")
+    }
+
+    return files
 }
--- gat/files_test.go
+++ gat/files_test.go
@@ -11,6 +11,9 @@ var fileTests = []struct {
 }{
     {"files_test.go", []string{"files_test.go"}},
     {"files.go", []string{"files_test.go"}},
+    {"files_darwin.go", []string{"files_darwin_test.go", "files_test.go"}},
+    {"files_amd64.go", []string{"files_amd64_test.go", "files_test.go"}},
+    {"files_windows_386.go", []string{"files_windows_386_test.go", "files_windows_test.go", "files_test.go"}},
 }
 
 func (s *GatSuite) TestTestFiles(c *C) {
