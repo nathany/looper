68a4459698968908b0fc3dc6483c53976c593d8f
Nathan Youngman <git@nathany.com>
2014-12-12 20:37:24 -0700

    not much point using go check for two tests.

--- input_test.go
+++ input_test.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-	. "gopkg.in/check.v1"
+	"testing"
 )
 
-func (s *LooperSuite) TestMixedCaseCommand(c *C) {
-	c.Assert(NormalizeCommand(" Exit"), Equals, EXIT)
-}
+func TestNormalizeCommand(t *testing.T) {
+	commands := []struct {
+		input string
+		cmd   Command
+	}{
+		{" Exit", EXIT},
+		{"sudo", UNKNOWN},
+	}
 
-func (s *LooperSuite) TestUnkownCommand(c *C) {
-	c.Assert(NormalizeCommand("sudo"), Equals, UNKNOWN)
+	for _, c := range commands {
+		actual := NormalizeCommand(c.input)
+		if actual != c.cmd {
+			t.Errorf("Expected '%s' to result in %v, but got %v", c.input, c.cmd, actual)
+		}
+	}
 }
