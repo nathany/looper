193b1bf317ee1dab1bc8b881a90c6613db1756c5
Alexander F Rødseth <rodseth@gmail.com>
2015-05-12 15:14:19 +0200

    Using the recwatch package

--- watch.go
+++ watch.go
@@ -1,51 +1,15 @@
 package main
 
 import (
-	"errors"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/go-fsnotify/fsnotify"
+	"github.com/xyproto/recwatch"
 )
 
-type RecursiveWatcher struct {
-	*fsnotify.Watcher
-	Files   chan string
-	Folders chan string
-}
-
-func NewRecursiveWatcher(path string) (*RecursiveWatcher, error) {
-	folders := Subfolders(path)
-	if len(folders) == 0 {
-		return nil, errors.New("No folders to watch.")
-	}
-
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		return nil, err
-	}
-	rw := &RecursiveWatcher{Watcher: watcher}
-
-	rw.Files = make(chan string, 10)
-	rw.Folders = make(chan string, len(folders))
-
-	for _, folder := range folders {
-		rw.AddFolder(folder)
-	}
-	return rw, nil
-}
-
-func (watcher *RecursiveWatcher) AddFolder(folder string) {
-	err := watcher.Add(folder)
-	if err != nil {
-		log.Println("Error watching: ", folder, err)
-	}
-	watcher.Folders <- folder
-}
-
-func (watcher *RecursiveWatcher) Run(debug bool) {
+func Run(watcher *recwatch.RecursiveWatcher, debug bool) {
 	go func() {
 		for {
 			select {
@@ -62,7 +26,7 @@ func (watcher *RecursiveWatcher) Run(debug bool) {
 						if debug {
 							DebugMessage("Detected new directory %s", event.Name)
 						}
-						if !shouldIgnoreFile(filepath.Base(event.Name)) {
+						if !recwatch.ShouldIgnoreFile(filepath.Base(event.Name)) {
 							watcher.AddFolder(event.Name)
 						}
 					} else {
@@ -86,30 +50,4 @@ func (watcher *RecursiveWatcher) Run(debug bool) {
 			}
 		}
 	}()
-}
-
-// Subfolders returns a slice of subfolders (recursive), including the folder provided.
-func Subfolders(path string) (paths []string) {
-	filepath.Walk(path, func(newPath string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if info.IsDir() {
-			name := info.Name()
-			// skip folders that begin with a dot
-			if shouldIgnoreFile(name) && name != "." && name != ".." {
-				return filepath.SkipDir
-			}
-			paths = append(paths, newPath)
-		}
-		return nil
-	})
-	return paths
-}
-
-// shouldIgnoreFile determines if a file should be ignored.
-// File names that begin with "." or "_" are ignored by the go tool.
-func shouldIgnoreFile(name string) bool {
-	return strings.HasPrefix(name, ".") || strings.HasPrefix(name, "_")
 }
